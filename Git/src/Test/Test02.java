68bda4d3724d2dfed00b86c1546dc7d4fa1fbaf3
rnzusl0412 <rnzusl0412@gmail.com>
2019-01-19 17:43:53 +0900

    EMP 호출

--- Git/src/Test/Test02.java
+++ Git/src/Test/Test02.java
@@ -2,9 +2,22 @@ package Test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Test02 {
 	public static void main(String[] args) throws Exception {
 		Connection c = DriverManager.getConnection("127.0.0.1", "id", "password");
+		Statement stmt = c.createStatement();
+		String sql = "SELECT * FROM EMP;";
+		ResultSet rs = stmt.executeQuery(sql);
+
+		while (rs.next()) {
+			System.out.println(rs.getString(1) + "\t" + rs.getString(2));
+		}
+
+		rs.close();
+		stmt.close();
+		c.close();
 	}
 }
